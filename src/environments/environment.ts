b27c52788eaddbe23160af37ab11871370ba3eb1
renanxd25 <renanxd7x@hotmail.com>
2024-11-10 06:53:57 -0300

    arquivo de produção

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebaseConfig: {
     apiKey: "AIzaSyDv3g5xPhqKZTSNZ7b8ywXAcoEko5H2Hfo",
     authDomain: "ionictesteb2b.firebaseapp.com",
