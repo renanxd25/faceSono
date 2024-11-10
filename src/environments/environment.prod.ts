97fa089ce3b560057d1077587da1ef9595737aa0
renanxd25 <renanxd7x@hotmail.com>
2024-11-10 07:02:08 -0300

    Configurando ambiente firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDv3g5xPhqKZTSNZ7b8ywXAcoEko5H2Hfo",
+    authDomain: "ionictesteb2b.firebaseapp.com",
+    projectId: "ionictesteb2b",
+    storageBucket: "ionictesteb2b.firebasestorage.app",
+    messagingSenderId: "150528207433",
+    appId: "1:150528207433:web:5144a666ef51d6b7536d98",
+    measurementId: "G-2R89VLD3FT"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   firebaseConfig: {
     apiKey: "AIzaSyDv3g5xPhqKZTSNZ7b8ywXAcoEko5H2Hfo",
     authDomain: "ionictesteb2b.firebaseapp.com",
